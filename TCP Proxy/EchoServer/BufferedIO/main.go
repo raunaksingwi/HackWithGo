b009aa2eb3e90393fe3010f91c7a31c7d1e770a1
Raunak Singwi <raunaksingwi7@gmail.com>
2020-08-25 16:37:47 +0530

    Reorg and add echo server

---
+++ TCP Proxy/EchoServer/BufferedIO/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"net"
+)
+
+func echo(conn net.Conn) {
+	defer conn.Close()
+
+	reader := bufio.NewReader(conn)
+	s, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatalln("Unable to read date")
+	}
+	log.Printf("Read %d bytes: %s", len(s), s)
+
+	log.Println("Writing data")
+	writer := bufio.NewWriter(conn)
+	if _, err := writer.WriteString(s); err != nil {
+		log.Fatalln("Unable to write data")
+	}
+	writer.Flush()
+}
