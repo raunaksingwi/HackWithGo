76c6af83926f6fbaf745dd686239b87b4b90a3f9
Raunak Singwi <raunaksingwi7@gmail.com>
2020-08-28 12:51:17 +0530

    Add main function

--- TCP Proxy/EchoServer/BufferedIO/main.go
+++ TCP Proxy/EchoServer/BufferedIO/main.go
@@ -23,3 +23,23 @@ func echo(conn net.Conn) {
 	}
 	writer.Flush()
 }
+
+func main() {
+	// Bind to TCP port 20080 on all interfaces.
+	listener, err := net.Listen("tcp", ":20080")
+	if err != nil {
+		log.Fatalln("Unable to bind to port")
+	}
+	log.Println("Listening on 0.0.0.0:20080")
+
+	for {
+		// Wait for connection. Create net.Conn on connection established.
+		conn, err := listener.Accept()
+		log.Println("Received connection")
+		if err != nil {
+			log.Fatalln("Unable to accept connection")
+		}
+		// Handle the connection. Using goroutine for concurrency.
+		go echo(conn)
+	}
+}
